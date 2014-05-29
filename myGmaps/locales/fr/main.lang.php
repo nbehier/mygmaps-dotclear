a7819f297b00a658b0368a7c1a46db5ed3cc027a
padawan83 <phililippe@dissitou.org>
2014-05-29 09:10:00 +0200

    Formatted translations files

---
+++ myGmaps/locales/fr/main.lang.php
@@ -0,0 +1,339 @@
+<?php
+// Language: Français 
+// Module: myGmaps - 4.4
+// Date: 2014-05-29 07:04:25 
+// Translated with dcTranslater - 2013.05.11 
+
+#_admin.php:17
+#_admin.php:48
+#add.map.php:455
+#config.maps.php:23
+#config.maps.php:373
+#element.map.php:525
+#inc/class.dcactionmaps.php:21
+#inc/class.dcactionmaps.php:36
+#inc/class.dcactionmaps.php:43
+$GLOBALS['__l10n']['Google Maps'] = 'Cartes Google';
+
+#_admin.php:129
+$GLOBALS['__l10n']['Are you sure you want to remove this map?'] = 'Êtes-vous certain de vouloir supprimer la carte ?';
+
+#_admin.php:135
+$GLOBALS['__l10n']['Are you sure you want to remove this element?'] = 'Êtes-vous certain de vouloir supprimer cet élément ?';
+
+#_admin.php:170
+#_admin.php:181
+#_admin.php:195
+$GLOBALS['__l10n']['Google Map:'] = 'Carte Google';
+
+#_admin.php:172
+$GLOBALS['__l10n']['No map'] = 'Pas de carte';
+
+#_admin.php:173
+#add.map.php:25
+$GLOBALS['__l10n']['Add a map to entry'] = 'Ajouter une carte au billet';
+
+#_admin.php:183
+$GLOBALS['__l10n']['Empty map'] = 'Carte vide';
+
+#_admin.php:184
+#_admin.php:217
+#add.map.php:52
+$GLOBALS['__l10n']['Edit map'] = 'Modifier la carte';
+
+#_admin.php:185
+#_admin.php:218
+$GLOBALS['__l10n']['Remove map'] = 'Supprimer la carte';
+
+#_admin.php:214
+$GLOBALS['__l10n']['Included elements list'] = 'Liste des éléments inclus';
+
+#_public.php:463
+$GLOBALS['__l10n']['Sorry, javascript must be activated in your browser to see this map.'] = 'Désolé, javascript doit être activé dans votre navigateur pour voir cette carte.';
+
+#add.map.php:151
+#config.maps.php:120
+$GLOBALS['__l10n']['none'] = 'non défini';
+
+#add.map.php:152
+#config.maps.php:121
+$GLOBALS['__l10n']['point of interest'] = 'point d\'intérêt';
+
+#add.map.php:153
+#config.maps.php:122
+$GLOBALS['__l10n']['polyline'] = 'ligne brisée';
+
+#add.map.php:154
+#config.maps.php:123
+$GLOBALS['__l10n']['polygon'] = 'polygone';
+
+#add.map.php:155
+#config.maps.php:124
+$GLOBALS['__l10n']['rectangle'] = 'rectangle';
+
+#add.map.php:156
+#config.maps.php:125
+$GLOBALS['__l10n']['circle'] = 'cercle';
+
+#add.map.php:157
+#config.maps.php:126
+$GLOBALS['__l10n']['included kml file'] = 'fichier kml inclus';
+
+#add.map.php:158
+#config.maps.php:127
+$GLOBALS['__l10n']['GeoRSS feed'] = 'flux GeoRSS';
+
+#add.map.php:159
+#config.maps.php:128
+$GLOBALS['__l10n']['directions'] = 'itinéraire';
+
+#add.map.php:160
+#config.maps.php:129
+$GLOBALS['__l10n']['weather'] = 'météo';
+
+#add.map.php:459
+$GLOBALS['__l10n']['Add elements'] = 'Ajouter des éléments';
+
+#add.map.php:460
+$GLOBALS['__l10n']['Select map elements for map attached to post:'] = 'Choisir les éléments de la carte attachée au billet :';
+
+#add.map.php:518
+#config.maps.php:438
+$GLOBALS['__l10n']['Map elements per page'] = 'Éléments de carte par page';
+
+#add.map.php:550
+$GLOBALS['__l10n']['Add selected map elements'] = 'Ajouter les éléments de carte sélectionnés';
+
+#add.map.php:572
+#add.map.php:598
+$GLOBALS['__l10n']['Map parameters'] = 'Paramètres de la carte';
+
+#add.map.php:573
+$GLOBALS['__l10n']['Choose options for map attached to post:'] = 'Choisir les options pour la carte attachée au billet :';
+
+#add.map.php:603
+#config.maps.php:496
+$GLOBALS['__l10n']['Choose default map center by moving target marker or searching a location. Choose zoom level and map type by using map controls.'] = 'Choisir le centre de la carte en déplaçant la cible ou en cherchant une adresse. Choisir le niveau de zoom et le type de carte en utilisant les contrôles de la carte.';
+
+#config.maps.php:385
+$GLOBALS['__l10n']['Configuration has been saved.'] = 'La configuration a été mise à jour.';
+
+#config.maps.php:386
+$GLOBALS['__l10n']['Elements status has been successfully updated'] = 'Le statut des éléments choisis a été mis à jour';
+
+#config.maps.php:387
+$GLOBALS['__l10n']['Elements have been successfully marked as selected'] = 'Les éléments choisis ont été marqués comme sélectionnés';
+
+#config.maps.php:388
+$GLOBALS['__l10n']['Elements have been successfully marked as deselected'] = 'Les éléments choisis ont été marqués comme désélectionnés';
+
+#config.maps.php:389
+$GLOBALS['__l10n']['Elements have been successfully deleted'] = 'Les éléments choisis ont supprimés';
+
+#config.maps.php:390
+$GLOBALS['__l10n']['Elements category has been successfully changed'] = 'La catégorie des éléments choisis a été changée';
+
+#config.maps.php:391
+$GLOBALS['__l10n']['Elements author has been successfully changed'] = 'L\'auteur des éléments choisis a été changé';
+
+#config.maps.php:392
+$GLOBALS['__l10n']['Elements language has been successfully changed'] = 'La langue des éléments choisis a été changée';
+
+#config.maps.php:401
+$GLOBALS['__l10n']['Map elements'] = 'Éléments de carte';
+
+#config.maps.php:403
+$GLOBALS['__l10n']['New element'] = 'Nouvel élément';
+
+#config.maps.php:459
+$GLOBALS['__l10n']['Selected map elements action:'] = 'Action sur les éléments de carte sélectionnés :';
+
+#config.maps.php:484
+$GLOBALS['__l10n']['Settings'] = 'Paramètres';
+
+#config.maps.php:489
+$GLOBALS['__l10n']['Enable extension for this blog'] = 'Activer l\'extension pour ce blog';
+
+#config.maps.php:491
+$GLOBALS['__l10n']['Default map options'] = 'Options des cartes par défaut';
+
+#element.map.php:46
+#element.map.php:688
+$GLOBALS['__l10n']['New map element'] = 'Nouvel élément de carte';
+
+#element.map.php:154
+$GLOBALS['__l10n']['This map element does not exist.'] = 'Cet élément de carte n\'existe pas';
+
+#element.map.php:179
+#element.map.php:688
+#element.map.php:693
+#inc/lib.pager.php:242
+$GLOBALS['__l10n']['Edit map element'] = 'Éditer l\'élément de carte';
+
+#element.map.php:189
+$GLOBALS['__l10n']['Next element'] = 'Élément suivant';
+
+#element.map.php:196
+$GLOBALS['__l10n']['Previous element'] = 'Élément précédent';
+
+#element.map.php:278
+#element.map.php:278
+#element.map.php:280
+#element.map.php:283
+$GLOBALS['__l10n']['No description.'] = 'Pas de description.';
+
+#element.map.php:473
+$GLOBALS['__l10n']['Stroke color'] = 'Couleur du trait';
+
+#element.map.php:474
+$GLOBALS['__l10n']['Stroke opacity'] = 'Opacité du trait';
+
+#element.map.php:475
+$GLOBALS['__l10n']['Stroke weight'] = 'Épaisseur du trait';
+
+#element.map.php:476
+$GLOBALS['__l10n']['Fill color'] = 'Couleur du fond';
+
+#element.map.php:477
+$GLOBALS['__l10n']['Fill opacity'] = 'Opacité du fond';
+
+#element.map.php:478
+$GLOBALS['__l10n']['Default icons'] = 'Icônes par défaut';
+
+#element.map.php:479
+$GLOBALS['__l10n']['Custom icons'] = 'Icônes personnelles';
+
+#element.map.php:482
+$GLOBALS['__l10n']['Custom Kml files'] = 'Fichiers kml personnels';
+
+#element.map.php:483
+$GLOBALS['__l10n']['Start:'] = 'Départ :';
+
+#element.map.php:484
+$GLOBALS['__l10n']['End:'] = 'Arrivée :';
+
+#element.map.php:543
+$GLOBALS['__l10n']['Map element has been updated.'] = 'L\'élément de carte a été mis à jour.';
+
+#element.map.php:546
+$GLOBALS['__l10n']['Map element has been created.'] = 'L\'élément de carte a été créé.';
+
+#element.map.php:576
+$GLOBALS['__l10n']['Map element status'] = 'État de l\'élément';
+
+#element.map.php:584
+$GLOBALS['__l10n']['Element language'] = 'Langue de l\'élément';
+
+#element.map.php:601
+$GLOBALS['__l10n']['Selected element'] = 'Élément sélectionné';
+
+#element.map.php:645
+$GLOBALS['__l10n']['Position:'] = 'Position :';
+
+#element.map.php:650
+#element.map.php:650
+$GLOBALS['__l10n']['Point of interest'] = 'Point d\'intérêt';
+
+#element.map.php:651
+#element.map.php:651
+$GLOBALS['__l10n']['Polyline'] = 'Ligne brisée';
+
+#element.map.php:652
+#element.map.php:652
+$GLOBALS['__l10n']['Polygon'] = 'Polygone';
+
+#element.map.php:654
+#element.map.php:654
+$GLOBALS['__l10n']['Circle'] = 'Cercle';
+
+#element.map.php:655
+#element.map.php:655
+$GLOBALS['__l10n']['Included Kml file'] = 'Fichier Kml inclus';
+
+#element.map.php:656
+#element.map.php:656
+$GLOBALS['__l10n']['GeoRSS Feed'] = 'Flux GeoRSS';
+
+#element.map.php:657
+#element.map.php:657
+$GLOBALS['__l10n']['Directions'] = 'Itinéraire';
+
+#element.map.php:658
+#element.map.php:658
+$GLOBALS['__l10n']['Weather'] = 'Météo';
+
+#element.map.php:659
+#element.map.php:659
+$GLOBALS['__l10n']['Initialize map'] = 'Initialiser la carte';
+
+#element.map.php:662
+$GLOBALS['__l10n']['This map will not be displayed on the blog. It is here only for positioning your element.'] = 'Cette carte ne sera pas affichée sur le blog, elle n\'est destinée qu\'à positionner votre élément.';
+
+#inc/class.dcactionmaps.php:49
+$GLOBALS['__l10n']['Back to map elements list'] = 'Retour à la liste des éléments';
+
+#inc/class.dcactionmaps.php:163
+#inc/class.dcactionmaps.php:173
+#inc/class.dcactionmaps.php:189
+#inc/class.dcactionmaps.php:210
+#inc/class.dcactionmaps.php:284
+#inc/class.dcactionmaps.php:339
+$GLOBALS['__l10n']['No element selected'] = 'Pas d\'élement sélectionné';
+
+#inc/class.dcactionmaps.php:259
+$GLOBALS['__l10n']['Create a new category for the element(s)'] = 'Créer une nouvelle catégorie pour le(s) éléments(s)';
+
+#inc/class.dcactionmaps.php:376
+$GLOBALS['__l10n']['Element language:'] = 'Langue de l\'élément :';
+
+#inc/lib.pager.php:159
+$GLOBALS['__l10n']['Elements list'] = 'Liste des éléments';
+
+#inc/lib.pager.php:247
+$GLOBALS['__l10n']['Remove map element'] = 'Supprimer l\'élément';
+
+$GLOBALS['__l10n']['Map'] = 'Carte';
+
+$GLOBALS['__l10n']['Maps'] = 'Cartes';
+
+$GLOBALS['__l10n']['Google Map'] = 'Carte Google :';
+
+$GLOBALS['__l10n']['Map content'] = 'Contenu de la carte';
+
+$GLOBALS['__l10n']['Map element published on:'] = 'Elément de carte publié le :';
+
+$GLOBALS['__l10n']['Insert map'] = 'Insérer une carte';
+
+$GLOBALS['__l10n']['Change category for selected elements'] = 'Changer la catégorie des éléments sélectionnés';
+
+$GLOBALS['__l10n']['Change author for selected elements'] = 'Changer l\'auteur des éléments sélectionnés';
+
+$GLOBALS['__l10n']['Change lang for selected elements'] = 'Changer la langue des éléments sélectionnés';
+
+$GLOBALS['__l10n']['Choose map center, zoom level and map type. This map will be displayed on the blog with all added elements.'] = 'Choisir le centre de la carte, le niveau de zoom et le type de carte. Cette carte sera affichée sur le blog avec les éléments que vous y aurez ajoutés.';
+
+$GLOBALS['__l10n']['View map'] = 'Voir la carte';
+
+$GLOBALS['__l10n']['Include kml file'] = 'Inclure un fichier kml';
+
+$GLOBALS['__l10n']['This map elements'] = 'Éléments de la carte';
+
+$GLOBALS['__l10n']['Info window content:'] = 'Contenu de l\'infobulle';
+
+$GLOBALS['__l10n']['Map element type'] = 'Type d\'élément';
+
+$GLOBALS['__l10n']['Map element type:'] = 'Type d\'élément :';
+
+$GLOBALS['__l10n']['No element'] = 'Pas d\'élément';
+
+$GLOBALS['__l10n']['No element (empty map)'] = 'Pas d\'élément (carte vide)';
+
+$GLOBALS['__l10n']['Add a map'] = 'Ajouter une carte';
+
+$GLOBALS['__l10n']['Add a map to page'] = 'Ajouter une carte à la page';
+
+$GLOBALS['__l10n']['Default map parameters'] = 'Paramètres des cartes par défaut';
+
+$GLOBALS['__l10n']['Radius'] = 'Rayon';
+
+?>
